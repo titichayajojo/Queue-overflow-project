094aad9406b4bb312482f45bce7e756e5271b63d
chanok <Chanok_boon@hotmail.com>
2021-10-25 01:10:50 +0700

    :zap: improve folder type and add router

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGripLinesVertical } from "@fortawesome/free-solid-svg-icons";
+import { ProfileLink, LogoLink } from "../Link/Link";
+import { StyledHeader } from "../Header/HeaderStyle";
+import { SearchInput } from "../Input/Inputs";
+import { NavLink, Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <StyledHeader>
+      <LogoLink to="/LoginPage">
+        <FontAwesomeIcon icon={faGripLinesVertical} size="2x" />
+        <span>
+          Queue<b>overflow</b>
+        </span>
+      </LogoLink>
+      <form action="" className="search">
+        <SearchInput type="text" placeholder="Search..."></SearchInput>
+      </form>
+      <ProfileLink to="/LoginPage">Login</ProfileLink>
+    </StyledHeader>
+  );
+}
+
+export default Header;
---
+++ src/Screens/LoginPage.js
@@ -0,0 +1,11 @@
+import Header from "../Components/Header/Header";
+
+function LoginPage() {
+  return (
+    <div>
+      <div>LoginPage</div>
+    </div>
+  );
+}
+
+export default LoginPage;
