4b75cf2ee5c7ea73ab6b143dcf1c13a78d6a3102
Ojo <titichaya_love@hotmail.com>
2021-10-26 00:43:11 +0700

    [FIX] change background color of writer

--- src/Screens/ViewQuestionPage.js
+++ src/Screens/ViewQuestionPage.js
@@ -49,16 +49,14 @@ const WriterRow = styled.div`
   justify-content: center;
   align-items: center;
   margin-left: 570px;
-  
-  
 `;
 
 const Writer = styled.div`
-    width: 190px;
-    height: 67px;
-    background-color: hsl(206, 53%, 35%);
-    padding-left: 10px;
-  
+  width: 190px;
+  height: 67px;
+  background-color: hsl(206, 46%, 30%);
+  padding-left: 10px;
+  padding-bottom: 5px;
 `;
 
 const AskedDate = styled.div`
@@ -66,7 +64,6 @@ const AskedDate = styled.div`
   padding-top: 10px;
   color: hsl(210deg 8% 70%);
   font-size: 0.8rem;
-
 `;
 
 const WriterName = styled.span`
@@ -101,84 +98,10 @@ function ViewQuestionPage() {
           place I need to fetch 3 different assets for component and I decided
           to handle change of status with useState because extracting 3
           different statuses from redux and combining it will be ugly, with
-          useState is much cleaner i think) Thanks in advance for help! :) I
-          need to test catch when fetching data request rejects but I don't
-          understand why error is not catched and I recieved this error:
-          UnhandledPromiseRejectionWarning: Unhandled promise rejection. This
-          error originated either by throwing inside of an async function
-          without a catch block, or by rejecting a promise which was not handled
-          with .catch(). (rejection id: 2) If I test case when fetchSomething
-          resolves with Promise.resolve() evertyhing works fine and tests are
-          correct but when I try to Promise.reject() in order to test catch case
-          then this error is not catched and I have unhandled promise rejection.
-          (Why code looks like this: In other places in the app I handle
-          changing of status with redux, so testing of catch is easy, but in one
-          place I need to fetch 3 different assets for component and I decided
-          to handle change of status with useState because extracting 3
-          different statuses from redux and combining it will be ugly, with
-          useState is much cleaner i think) Thanks in advance for help! :) I
-          need to test catch when fetching data request rejects but I don't
-          understand why error is not catched and I recieved this error:
-          UnhandledPromiseRejectionWarning: Unhandled promise rejection. This
-          error originated either by throwing inside of an async function
-          without a catch block, or by rejecting a promise which was not handled
-          with .catch(). (rejection id: 2) If I test case when fetchSomething
-          resolves with Promise.resolve() evertyhing works fine and tests are
-          correct but when I try to Promise.reject() in order to test catch case
-          then this error is not catched and I have unhandled promise rejection.
-          (Why code looks like this: In other places in the app I handle
-          changing of status with redux, so testing of catch is easy, but in one
-          place I need to fetch 3 different assets for component and I decided
-          to handle change of status with useState because extracting 3
-          different statuses from redux and combining it will be ugly, with
-          useState is much cleaner i think) Thanks in advance for help! :) I
-          need to test catch when fetching data request rejects but I don't
-          understand why error is not catched and I recieved this error:
-          UnhandledPromiseRejectionWarning: Unhandled promise rejection. This
-          error originated either by throwing inside of an async function
-          without a catch block, or by rejecting a promise which was not handled
-          with .catch(). (rejection id: 2) If I test case when fetchSomething
-          resolves with Promise.resolve() evertyhing works fine and tests are
-          correct but when I try to Promise.reject() in order to test catch case
-          then this error is not catched and I have unhandled promise rejection.
-          (Why code looks like this: In other places in the app I handle
-          changing of status with redux, so testing of catch is easy, but in one
-          place I need to fetch 3 different assets for component and I decided
-          to handle change of status with useState because extracting 3
-          different statuses from redux and combining it will be ugly, with
-          useState is much cleaner i think) Thanks in advance for help! :) I
-          need to test catch when fetching data request rejects but I don't
-          understand why error is not catched and I recieved this error:
-          UnhandledPromiseRejectionWarning: Unhandled promise rejection. This
-          error originated either by throwing inside of an async function
-          without a catch block, or by rejecting a promise which was not handled
-          with .catch(). (rejection id: 2) If I test case when fetchSomething
-          resolves with Promise.resolve() evertyhing works fine and tests are
-          correct but when I try to Promise.reject() in order to test catch case
-          then this error is not catched and I have unhandled promise rejection.
-          (Why code looks like this: In other places in the app I handle
-          changing of status with redux, so testing of catch is easy, but in one
-          place I need to fetch 3 different assets for component and I decided
-          to handle change of status with useState because extracting 3
-          different statuses from redux and combining it will be ugly, with
-          useState is much cleaner i think) Thanks in advance for help! :) I
-          need to test catch when fetching data request rejects but I don't
-          understand why error is not catched and I recieved this error:
-          UnhandledPromiseRejectionWarning: Unhandled promise rejection. This
-          error originated either by throwing inside of an async function
-          without a catch block, or by rejecting a promise which was not handled
-          with .catch(). (rejection id: 2) If I test case when fetchSomething
-          resolves with Promise.resolve() evertyhing works fine and tests are
-          correct but when I try to Promise.reject() in order to test catch case
-          then this error is not catched and I have unhandled promise rejection.
-          (Why code looks like this: In other places in the app I handle
-          changing of status with redux, so testing of catch is easy, but in one
-          place I need to fetch 3 different assets for component and I decided
-          to handle change of status with useState because extracting 3
-          different statuses from redux and combining it will be ugly, with
           useState is much cleaner i think) Thanks in advance for help! :)
         </span>
       </VotesRow>
+
       <TagRow>
         <Tag>java</Tag>
         <Tag>react.js</Tag>
@@ -189,15 +112,12 @@ function ViewQuestionPage() {
       </TagRow>
 
       <WriterRow>
-          
         <Writer>
-        <AskedDate>asked May 27 '20 at 8:06</AskedDate>
-        <FontAwesomeIcon icon={faPaw} size="2x" />
-        <WriterName>Darek Gala</WriterName>
+          <AskedDate>asked May 27 '20 at 8:06</AskedDate>
+          <FontAwesomeIcon icon={faPaw} size="2x" />
+          <WriterName>Darek Gala</WriterName>
         </Writer>
       </WriterRow>
-
-      
     </div>
   );
 }
