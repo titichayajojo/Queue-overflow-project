4ef01b0513c9d3e0a3ddf1e21f3e917156d2237a
Ojo <titichaya_love@hotmail.com>
2021-10-29 14:40:41 +0700

    [ADD] method get and post tag

--- src/django-postgres/env/postgresTest/queueOverflowDB/views.py
+++ src/django-postgres/env/postgresTest/queueOverflowDB/views.py
@@ -3,8 +3,9 @@ from django.db import reset_queries
 from django.http import response
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import TeacherSerializer, QuestionSerializer
+from .serializers import TeacherSerializer, QuestionSerializer, TagSerializer
 from .Model.Question import Teacher, Question
+from .Model.Tag import Tag
 from rest_framework.decorators import api_view
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
@@ -27,6 +28,7 @@ def getAllTeachers(request):
 
 @api_view(['GET', 'POST', 'DELETE'])
 def questionsList(request):
+    # get all questions
     if request.method == 'GET':
         questions = list(Question.objects.all().order_by('createdAt'))
         
@@ -39,24 +41,44 @@ def questionsList(request):
             res.append(element)
             
         return JsonResponse(res, safe=False)
-    
+
+    # post a question
     elif request.method == 'POST':
-        question_data = JSONParser().parse(request)
-        question_serializer = QuestionSerializer(data=question_data)
-        if question_serializer.is_valid():
-            question_serializer.save()
-            return JsonResponse(question_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(question_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        questionData = JSONParser().parse(request)
+        questionSerializer = QuestionSerializer(data=questionData)
+        if questionSerializer.is_valid():
+            questionSerializer.save()
+            return JsonResponse(questionSerializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(questionSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET', 'POST', 'DELETE'])
 def questionDetail(request, id):
+    # get question by id
     if request.method == 'GET':
         question = Question.objects.get(id=id)
-        question_serializer = QuestionSerializer(question) 
-        dict = question_serializer.data
+        questionSerializer = QuestionSerializer(question) 
+        dict = questionSerializer.data
         res = []
         element = json.loads(json.dumps(dict))
         element["askedDate"] = calculateNDaysAgo(element.get('createdAt'))
         res.append(element)
         return JsonResponse(res, safe=False)
+
+@api_view(['GET', 'POST', 'DELETE'])
+def tagList(request):
+    # get all tags
+    if request.method == 'GET':
+        tags = Tag.objects.all()
+        tagSerializer = TagSerializer(tags) 
+        return JsonResponse(tagSerializer.data, safe=False)
+
+    # post a tag
+    elif request.method == 'POST':
+        tagData = JSONParser().parse(request)
+        tagSerializer = TagSerializer(data=tagData)
+        if tagSerializer.is_valid():
+            tagSerializer.save()
+            return JsonResponse(tagSerializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(tagSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
             
