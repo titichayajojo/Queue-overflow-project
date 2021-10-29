792fdff4a1e6f3676a90ef7f098a596a01a321d6
Ojo <titichaya_love@hotmail.com>
2021-10-29 14:40:58 +0700

    [ADD] tag serializer

--- src/django-postgres/env/postgresTest/queueOverflowDB/serializers.py
+++ src/django-postgres/env/postgresTest/queueOverflowDB/serializers.py
@@ -1,6 +1,7 @@
 # queueOverflowDB/serializers.py
 from rest_framework import serializers
 from .Model.Question import Teacher, Question
+from .Model.Tag import Tag
 
 class TeacherSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -11,3 +12,9 @@ class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
         fields = ('id', 'title', 'body', 'votes', 'answers', 'views', 'tags', 'writer','createdAt')
+
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('title', 'description', 'createdAt')
