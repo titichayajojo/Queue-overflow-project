fc004148bd5777bd405ce109cb742a71080fae21
Ojo <titichaya_love@hotmail.com>
2021-10-29 14:41:38 +0700

    [ADD] register model tag

--- src/django-postgres/env/postgresTest/queueOverflowDB/admin.py
+++ src/django-postgres/env/postgresTest/queueOverflowDB/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .Model.Question import Question, Teacher
+from .Model.Tag import Tag
 
 # Register your models here.
 admin.site.register(Teacher)
 admin.site.register(Question)
+admin.site.register(Tag)
