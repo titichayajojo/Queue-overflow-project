fb7ce2b4971ed3cc92df04af5309050d129cb740
chanok <Chanok_boon@hotmail.com>
2021-11-13 15:33:27 +0700

    :zap:  connecting register and login

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,6 +1,7 @@
 import classes from "./SignUpPage.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGripLinesVertical } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 import {
   faComment,
   faPizzaSlice,
@@ -10,6 +11,12 @@ import {
 import NormalButton from "../Components/Button/NormalButton";
 import TextInput from "../Components/Input/TextInput";
 import Button from "@mui/material/Button";
+import { Password } from "@mui/icons-material";
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+import { useSelector, useDispatch } from "react-redux";
+//Register case
+
 const features = [
   {
     titile: "Get unstuck — ask a question",
@@ -47,6 +54,43 @@ const buttons = [
 ];
 
 const SignUp = (props) => {
+  const [firstName, setFirstName] = useState("test");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const registerHandler = (props) => {
+    var headers = {};
+    fetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      mode: "cors",
+      headers: headers,
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        username: userName,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((jsonResponse) => {
+        setLoading(false);
+        if (jsonResponse) {
+          alert("Register successfully");
+        } else {
+          alert("please check your email and password and try again or plesae");
+        }
+      })
+      .catch((error) => console.error(error, error.stack));
+  };
   return (
     <div className={classes.body}>
       <div className={classes.leftMain}>
@@ -85,19 +129,42 @@ const SignUp = (props) => {
             return <NormalButton item={item} />;
           })}
           <div className={classes.inputBox}>
-            <TextInput title={"Firstname"} />
-            <TextInput title={"Lastname"} />
-            <TextInput title={"Nickname"} />
-            <TextInput title={"Username"} />
-            <TextInput title={"Password"} />
+            <TextInput
+              title={"Firstname"}
+              value={firstName}
+              setValue={setFirstName}
+            />
+            <TextInput
+              title={"Lastname"}
+              value={lastName}
+              setValue={setLastName}
+            />
+            <TextInput title={"Email"} value={email} setValue={setEmail} />
+            <TextInput
+              title={"Username"}
+              value={userName}
+              setValue={setUserName}
+            />
+            <TextInput
+              title={"Password"}
+              value={password}
+              setValue={setPassword}
+            />
 
             <Button
+              onClick={() => {
+                setLoading(true);
+                registerHandler();
+              }}
               variant="contained"
               className={classes.loginButton}
               style={{ marginTop: 20, fontWeight: "bold" }}
             >
               Sign Up
             </Button>
+          </div>
+          <div style={{ marginTop: 20, marginLeft: 210 }}>
+            <ClipLoader color={"#0A95FF"} loading={loading} size={50} />
           </div>
         </div>
       </div>
